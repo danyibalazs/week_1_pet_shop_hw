8c94ee16e806563a94b633e4249b99d1053a4a9c
danyibalazs <danyi.balage@gmail.com>
2020-01-17 14:23:13 +0000

    12 pass

--- pet_shop.rb
+++ pet_shop.rb
@@ -38,4 +38,13 @@ def find_pet_by_name(pet_shop, name)
       return pet
     end
   end
+  return nil
+end
+
+def remove_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      pet_shop[:pets].delete(pet)
+    end
+  end
 end
